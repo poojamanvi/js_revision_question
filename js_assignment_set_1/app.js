4301cf9aebd4896a7a8e46ea3cea1beeffa36497
Pooja <poojadahiya121001@gmail.com>
2022-06-21 12:42:44 +0530

    add

--- js_assignment_set_1/app.js
+++ js_assignment_set_1/app.js
@@ -8,8 +8,8 @@ for(let i=1; i<=20;i++){
 
 }
 
-// Question-2
-// Print the sum of all even numbers in a range from a to b. (Including a,b)
+// // Question-2
+// // Print the sum of all even numbers in a range from a to b. (Including a,b)
 let sum=0;
 for(i=10;i<=30;i++){
      if(i%2==0){
@@ -21,33 +21,41 @@ console.log(sum)
 
 
 
-// Question-3
-// Write a program to read the first and last digit of a number
+// // Question-3
+// // Write a program to read the first and last digit of a number
 
-let num= 124356
+ num= 124356
 
-let lastD=num%10
-console.log("last digit",lastD)
+let lastDigit=num%10
+console.log("last digit",lastDigit)
 
-let firstD
+let firstDigit
+
 while(num>10){
-       firstD=Math.floor(num/10)
-       num/=10
-}
-console.log("first digit",firstD)
-
-// let lastD=num%10
-// console.log("last digit",lastD)
+       firstDigit=Math.floor(num/10)
+        num/=10
+ }
+console.log("first digit",firstDigit)
 
 
- 
+
 // Question-4
 // Check whether the given number is an armstrong number or not.
-//  num=153
-//  let lastDigit = num%10 //3
-//   let y=Math.floor(num/10)//15
-//   let secondDigit = y%10 //5
-  
+
+ num = 153;
+ sum=0;
+while(num>0){
+   let  reminder=(num%10)
+     sum=sum+reminder**3
+     num = Math.floor(num/10)
+}
+
+
+if(num==sum){
+    console.log(" The given number is an armstrong number")
+}else {
+    console.log(" The given number is an armstrong number")
+}
 
 // Question-5 
 // Take two numbers and find product of all numbers between them that satisfy following condition (inclusive range): 
@@ -65,13 +73,18 @@ for(i=40; i<=50;i++){
 console.log(product)
 
 
-// Question-6
-// Take a number and return all the even digits of the number.
-//  num =142
-//  x=num%10
-//  if(x%2==0){
-//      print even
-//  }
+// // Question-6
+// // Take a number and return all the even digits of the number.
+  num =1425289
+
+
+  while(num>0){
+     reminder = (num%10)
+    if(reminder%2==0)console.log(reminder)
+       num=Math.floor(num/10)
+
+}
+ 
 
 
 
